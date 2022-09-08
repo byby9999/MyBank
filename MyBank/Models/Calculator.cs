99165e940a7826f9f473dc7d9071f296992a5976
Alexandra Savescu <alexandra.savescu@maxcode.net>
2022-09-08 17:43:58 +0300

    Fixed code smells

--- MyBank/Models/Calculator.cs
+++ MyBank/Models/Calculator.cs
@@ -6,18 +6,16 @@
         {
             var age = DateTime.Now.Year - credit.Applicant.BirthDate.Year;
             if (age < 18)
-            return CreditStatus.Rejected;
-
-            if (credit.Amount > 50000)
             {
-                if (credit.Applicant.JobHistory?.Count() < 2) 
-                {
-                    return CreditStatus.ReferredToHuman;
-                }
+                return CreditStatus.Rejected;
+            }
+            if (credit.Amount > 50000 && credit.Applicant.JobHistory?.Count < 2)
+            {
+                return CreditStatus.ReferredToHuman;
             }
 
             if (credit.Amount > 100000 &&
-                (credit.Applicant.JobHistory == null || credit.Applicant.JobHistory.Count() == 0))
+                (credit.Applicant.JobHistory == null || !credit.Applicant.JobHistory.Any()))
                 return CreditStatus.Rejected;
 
             return CreditStatus.Accepted;
