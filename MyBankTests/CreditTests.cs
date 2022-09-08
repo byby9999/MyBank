14dc8bca53b75681ad5115f36e286b89af361aec
Alexandra Savescu <alexandra.savescu@maxcode.net>
2022-09-08 13:26:20 +0300

    Add tests for large amounts

--- MyBankTests/CreditTests.cs
+++ MyBankTests/CreditTests.cs
@@ -16,5 +16,56 @@ namespace MyBankTests
 
             Assert.Equal(CreditStatus.Rejected, result);
         }
+
+        [Fact]
+        public void Apply_JobHistoryWithMinimum2Jobs_ShouldAccept()
+        {
+            var applicant = new Person
+            {
+                BirthDate = new DateTime(2000, 12, 12),
+                JobHistory = new Dictionary<string, int> { { "Comp", 2 }, { "Frfee", 4 }, { "SUperX", 3 } }
+            };
+            var calculator = new Calculator();
+
+            var credit = new Credit { Amount = 3200, Applicant = applicant, Months = 20 };
+
+            var result = calculator.Apply(credit);
+
+            Assert.Equal(CreditStatus.Accepted, result);
+        }
+
+        [Fact]
+        public void Apply_LargeAmountAndNoJobHistory_ShouldAccept()
+        {
+            var applicant = new Person
+            {
+                BirthDate = new DateTime(2000, 12, 12)
+            };
+            var calculator = new Calculator();
+
+            var credit = new Credit { Amount = 80000, Applicant = applicant, Months = 20 };
+
+            var result = calculator.Apply(credit);
+
+            Assert.Equal(CreditStatus.Accepted, result);
+        }
+
+        [Fact]
+        public void Apply_LargeAmountAndOnFirstJob_ShouldReferToHuman()
+        {
+            var applicant = new Person
+            {
+                BirthDate = new DateTime(2000, 12, 12),
+                JobHistory = new Dictionary<string, int> { { "CompA", 12 } }
+            };
+            var calculator = new Calculator();
+
+            var credit = new Credit { Amount = 80000, Applicant = applicant, Months = 20 };
+
+            var result = calculator.Apply(credit);
+
+            Assert.Equal(CreditStatus.ReferredToHuman, result);
+        }
+
     }
 }
