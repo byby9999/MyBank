58b49190d4c9cb0509cdf4a42bd34be05ab1f99f
Alexandra Savescu <alexandra.savescu@maxcode.net>
2022-09-08 13:06:27 +0300

    Add new unit test

--- MyBankTests/CreditTests.cs
+++ MyBankTests/CreditTests.cs
@@ -16,5 +16,22 @@ namespace MyBankTests
 
             Assert.Equal(CreditStatus.Rejected, result);
         }
+
+        [Fact]
+        public void Apply_JobHistoryWithMinimum2Jobs_ShouldAccept()
+        {
+            var applicant = new Person
+            {
+                BirthDate = new DateTime(2000, 12, 12),
+                JobHistory = new Dictionary<string, int> { { "Comp", 2 }, { "Frfee", 4 }, { "SUperX", 3 } }
+            };
+            var calculator = new Calculator();
+
+            var credit = new Credit { Amount = 3200, Applicant = applicant, Months = 20 };
+
+            var result = calculator.Apply(credit);
+
+            Assert.Equal(CreditStatus.Accepted, result);
+        }
     }
 }
